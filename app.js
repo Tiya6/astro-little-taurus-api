848c04639c9857bfdc0b04789486ccccc73dcf97
Tiya6 <tiyaseiss@gmail.com>
2020-04-24 18:15:49 +0200

    updated cors

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ const whitelist = ["http://localhost:3000"];
 const corsOptions = {
   origin: function(origin, callback) {
     callback(null,true)
-    /*if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
-    }*/
+    }
   },
   credentials: true
 };
